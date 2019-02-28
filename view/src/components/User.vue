7226ab2d78768566effda24ce06eb2fe96bac687
Rossev Gleb <rossevgleb@gmail.com>
2019-02-28 19:02:15 +0300

    Users table added

---
+++ view/src/components/User.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="display-1 grey--text">{{user.name}}</div>
+    <v-data-table
+      v-if="user"
+      :items="user.periods"
+      :headers="headers"
+      selec-all
+      class="elevation-1"
+      header-text="user.name"
+    >
+      <template slot="headers" slot-scope="props">
+        <tr>
+          <th v-for="header in props.headers" :key="header.text">{{ header.text }}</th>
+        </tr>
+      </template>
+      <template slot="items" slot-scope="props">
+        <tr>
+          <td>{{props.item.from}} - {{props.item.to}}</td>
+          <td>{{props.item.status}}</td>
+          <td router :to="props.item.id">Show all</td>
+        </tr>
+      </template>
+    </v-data-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      headers: [{ text: "Period" }, { text: "Status" }, { text: "Show data" }]
+    };
+  },
+  props: {
+    user: Object
+  }
+};
+</script>
+
+<style>
+</style>
--- view/src/views/Users.vue
+++ view/src/views/Users.vue
@@ -1,10 +1,32 @@
 <template>
   <div class="users">
     <h1 class="subheading grey--text">Users</h1>
-    <v-container class="my-5">content</v-container>
+    <v-container class="my-5">
+      <User v-for="user in users" :key="user.id" v-bind:user="user"/>
+    </v-container>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+import User from "@/components/User";
+export default {
+  data() {
+    return {
+      users: []
+    };
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/")
+      .then(res => {
+        this.users = res.data;
+        console.log(this.users);
+      })
+      .catch(e => console.error(e));
+  },
+  components: {
+    User
+  }
+};
 </script>
