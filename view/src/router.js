085ae74fe3c02329032e536795ad90d5b64ff013
Rossev Gleb <rossevgleb@gmail.com>
2019-02-28 16:19:56 +0300

    Routes added

--- view/src/router.js
+++ view/src/router.js
@@ -1,15 +1,28 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Users from "./views/Users.vue";
+import Projects from "./views/Projects.vue";
+import Periods from "./views/Periods.vue";
 
 Vue.use(Router);
 
 export default new Router({
+  mode: "history",
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Home
+      name: "users",
+      component: Users
+    },
+    {
+      path: "/projects",
+      name: "projects",
+      component: Projects
+    },
+    {
+      path: "/periods",
+      name: "periods",
+      component: Periods
     }
   ]
 });
---
+++ view/src/views/Periods.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="periods">
+    <h1 class="subheading grey--text">Periods</h1>
+    <v-container class="my-5">content</v-container>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
