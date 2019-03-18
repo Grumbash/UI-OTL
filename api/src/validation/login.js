67f85f8afec82b5dc98b89e87ceb4a587f111b20
Rossev Gleb <rossevgleb@gmail.com>
2019-03-18 14:12:56 +0300

    Fix validation

--- api/src/validation/login.js
+++ api/src/validation/login.js
@@ -4,15 +4,15 @@ const isEmpty = require("./is-empty");
 module.exports = function validateLoginInput(data) {
   let errors = {};
 
-  data.email = !isEmpty(data.email) ? data.email : "";
+  data.login = !isEmpty(data.login) ? data.login : "";
   data.password = !isEmpty(data.password) ? data.password : "";
 
-  if (!Validator.isEmail(data.email)) {
-    errors.email = "Email is invalid";
+  if (!Validator.isEmail(data.login)) {
+    errors.login = "Login is invalid";
   }
 
-  if (Validator.isEmpty(data.email)) {
-    errors.email = "Email feild is required";
+  if (Validator.isEmpty(data.login)) {
+    errors.login = "Login feild is required";
   }
 
   if (Validator.isEmpty(data.password)) {
